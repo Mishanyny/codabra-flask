8dca934efcf09420c1cbea9c2da8de9d00fef7ca
Kovalev Mihail <maykl_1999@tut.by>
2018-11-08 23:59:52 +0300

    first

--- app/profile/models.py
+++ app/profile/models.py
@@ -1,12 +1,27 @@
 from .. import db
+from flask_login import UserMixin
+from app import login
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Document):
-
+class User(UserMixin, db.Document):
+    
     user_id = db.SequenceField()
+    password_hash = db.StringField()
     full_name = db.StringField()
-    nick_name = db.StringField()
+    username = db.StringField()
     city = db.StringField(default="")
     avatar_hash = db.StringField(default="")
     phone = db.StringField(default="")
     about_me = db.StringField(default="")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(user_id):
+    return User.objects(pk=user_id).first()
+
     
--- app/profile/views.py
+++ app/profile/views.py
@@ -1,19 +1,62 @@
 from . import profile
-from flask import render_template, flash, redirect
+from flask import render_template, redirect, session, url_for, g, flash
 from .models import User
-
+from .forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from .models import User
+from .forms import RegistrationForm
 
 NAME = 'Misha'
 
+
+@profile.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile.about'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.objects(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('profile.login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('profile.main_page'))
+    return render_template('login.html', form=form)
+
+
+@profile.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile.about'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        user.save()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('profile.login'))
+    return render_template('register.html', form=form)
+
+
+@profile.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('.about'))
+
+
+@profile.route('/about/')
 @profile.route('/')
 def about():
+    if not current_user.is_authenticated:
+        return redirect(url_for('profile.login'))
     return render_template("about.html", name = NAME)
 
 
-@profile.route('/main_page/<int:user_id>/')
-def main_page(user_id):
-    user = User.objects(user_id = user_id).first()
-    return render_template("profile.html", user = user)
+@profile.route('/main_page/')
+def main_page():
+    if not current_user.is_authenticated:
+        return redirect(url_for('profile.login'))
+    return render_template("profile.html", user = current_user)
 
 @profile.route('/message/')
 def message():
